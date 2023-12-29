3678c864166a7ca459bfed6867eb816357961687
Bianca Macedo <bianca.mhm1@gmail.com>
2023-12-29 15:33:33 +0000

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Calculadora de empréstimos',
+  icons: {
+    icon: "favicon.png",
+  },
 }
 
 export default function RootLayout({
